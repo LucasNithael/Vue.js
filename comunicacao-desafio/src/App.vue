5d3d9c6c4213510295d5f3181a0c0e6f878cc666
LucasNithael <lucasnithael0101@gmail.com>
2024-02-24 22:54:58 -0300

    desafio de comunicacão entre componentes

---
+++ comunicacao-desafio/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <usuario-padrao />
+
+</template>
+
+<script>
+  import UsuarioPadrao from './components/UsuarioPadrao.vue'
+export default {
+  components: { UsuarioPadrao }
+}
+</script>
+
+<style>
+
+</style>
