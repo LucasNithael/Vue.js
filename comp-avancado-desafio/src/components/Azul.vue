d7cf9487fd17c7c9db9aebc5ece7f2567bfc9e74
LucasNithael <lucasnithael0101@gmail.com>
2024-03-03 23:13:33 -0300

    components avançado

---
+++ comp-avancado-desafio/src/components/Azul.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="caixa azul">
+        <conteudo-azul>
+            Conteúdo do Componente Azul
+        </conteudo-azul>
+    </div>
+</template>
+
+<script>
+ import ConteudoAzul from './ConteudoAzul.vue'
+
+export default {
+    components: { ConteudoAzul }
+}
+</script>
+
+<style scoped>
+
+</style>
